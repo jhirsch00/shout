c6b62cb3feb1c7995129e38aef9f562bf572b3c4
Dan Croak <dcroak@thoughtbot.com>
2011-03-03 13:20:00 -0500

    a new rails app except for Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shout::Application.config.secret_token = '8fb3f0206a8d10545e4a7e254e1dce2dc9ce5cf5e62cfedc00d6b8e7034dee48129c86b9705fca0c06008f42ab1695a362af2f89eff76b197d3e28eb81a935ac'
